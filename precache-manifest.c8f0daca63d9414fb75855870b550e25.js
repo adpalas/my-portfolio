2bd71978c6cfe3582c896ec7636f3ae217651df3
Aaron Palas <aaron.palas@gmail.com>
2020-08-02 16:01:52 -0700

    Updates

---
+++ precache-manifest.c8f0daca63d9414fb75855870b550e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12f703c714fc5bbcf9f538747f7a7d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdca0c06270123be7fd0",
+    "url": "/static/css/main.66021776.chunk.css"
+  },
+  {
+    "revision": "2563805ff02a7583cf0f",
+    "url": "/static/js/2.bab48a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bab48a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdca0c06270123be7fd0",
+    "url": "/static/js/main.f34d372b.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "f99d28b8b5fb5c128ea99a49fad40d31",
+    "url": "/static/media/118-macbook-edited.f99d28b8.svg"
+  }
+]);
