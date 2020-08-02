2b4bd91136dca84bab7f470a0088c1a06b2516f5
Aaron Palas <aaron.palas@gmail.com>
2020-08-02 16:08:23 -0700

    Finaized protfolio. Added Resume link and deployment scripts for github pages.

--- src/components/aboutpage.jsx
+++ src/components/aboutpage.jsx
@@ -5,35 +5,35 @@ class Aboutpage extends Component {
 	render() {
 		return (
 			<div id="Aboutpage" className="min-vh-100 d-flex flex-column justify-content-center align-items-center">
-				<div className="Aboutpage-description container min-vw-80 min-vh-25 mb-5 border rounded">
+				<div className="Aboutpage-description container min-vw-80 min-vh-25 my-5 border rounded">
 					<h3 className="my-1">Hello! My name is <span class="Aboutpage-highlight">Aaron</span>.</h3>
 					<p>
-						I'm 26 years old and currently living in California. I graduated with a Bachelors degree in Computer Science and am now looking for opportunities in Web Development and Web Programming.
+						I'm 26 years old and currently living in California. I graduated with a Bachelors degree in Computer Science and am now looking for opportunities in Web Development.
 						<br></br>
 						<br></br>
-						Out of all the fields I studied in school, the topics in web technologies would always grab my attention the most. Now I have found my passion in building both websites and full-stack web applications. 
+						Out of all the fields I studied in school, the topics in web technologies would always grab my attention the most. Now I have found my passion in building full-stack web applications. 
 					</p>
 				</div>
 				<div className="Aboutpage-content container">
 					<h5>Here's what I've been working on...</h5>
 					<div className="card my-3 border-0">
 					  <div className="row no-gutters">
-						<div className="col-md-4 d-flex flex-column justify-content-center align-items-center">
+						<div className="col-sm-12 col-md-4 d-flex flex-column justify-content-center align-items-center">
 							<div className="d-flex justify-content-center align-items-center">
 								<h5 className="card-main-header card-title mb-0"><i class="fas fa-film"></i> IWIL</h5>
 							</div>
-							<div className="card-link-container card-body py-0 d-flex align-items-center">
+							<div className="card-link-containe py-0 d-flex align-items-center">
 								<div class="card-link-container-website h-100 d-flex justify-content-center align-items-center">
 									<a href="https://iwil.herokuapp.com/" class="card-link-website card-link">Go to Website</a>
 								</div>
-								<div className="Aboutpage-vertical-border h-75 mx-3 border rounded">
+								<div className="Aboutpage-vertical-border mx-3 border rounded">
 								</div>
 								<div class="card-link-container-github h-100 d-flex justify-content-center align-items-center">
 									<a href="https://github.com/adpalas/iwil" class="card-link-github card-link align-middle"><i class="fab fa-github"></i></a>
 								</div>
 							</div>
 						</div>
-						<div className="col-md-8">
+						<div className="col-sm-12 col-md-8">
 							<div className="card-body">
 								<h5 className="card-title">IWIL - I'll Watch It Later</h5>
 								<p className="card-text">A web application that utilizes web api to retrieve movie details and whether they are availble on streaming and/or web store plaforms. Successful searches can be selected and saved to a watchlist if desired.</p>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ class Footer extends Component {
 	render() {
 		return (
 			<div id="Footer">
-				<div className="container h-100 pt-5 d-flex flex-column align-items-center">
+				<div className="container h-100 pt-3 d-flex flex-column align-items-center">
 					<div className="Footer-links d-flex justify-content-center">
 						<div className="Footer-link-container d-flex justify-content-center align-items-center">
 							<a href="https://github.com/adpalas"><i class="fab fa-github"></i></a>
@@ -13,26 +13,25 @@ class Footer extends Component {
 						<div className="Footer-link-container d-flex justify-content-center align-items-center">
 							<a href="https://www.linkedin.com/in/aaron-palas-646511126/"><i class="fab fa-linkedin"></i></a>
 						</div>
+						<div className="Footer-link-container d-flex justify-content-center align-items-center">
+							<a href="/My Resume.docx.pdf"><i class="far fa-file"></i></a>
+						</div>
 					</div>
 					<div className="Footer-horizontal-border w-75 my-3 border rounded"></div>
-					<div className="Footer-contact-info d-flex justify-content-center">
-						<div className="d-flex">
+					<div className="Footer-contact-info w-75 row">
+						<div className="d-flex justify-content-center col-sm-12 col-md-4">
 							<div className="contact-info-icon d-flex align-items-center mr-2">
 								<i class="fas fa-thumbtack"></i>
 							</div>
 							<p className="mb-0">Long Beach, California</p>
 						</div>
-						<div className="Footer-vertical-border mx-5 border rounded">
-						</div>
-						<div className="d-flex">
+						<div className="d-flex justify-content-center col-sm-12 col-md-4">
 							<div className="contact-info-icon d-flex align-items-center mr-2">
 								<i class="far fa-envelope"></i>
 							</div>
 							<p className="mb-0">aaron.palas@gmail.com</p>
 						</div>
-						<div className="Footer-vertical-border mx-5 border rounded">
-						</div>
-						<div className="d-flex">
+						<div className="d-flex justify-content-center col-sm-12 col-md-4">
 							<div className="contact-info-icon d-flex align-items-center mr-2">
 								<i class="fas fa-phone"></i>
 							</div>
