ae8a7bbe1387a06c7717c72a5bdf7ff53114cad4
Aaron Palas <aaron.palas@gmail.com>
2021-04-12 21:38:33 -0700

    Updates

---
+++ precache-manifest.2dde23fdf2e9ca6c59abd1bf9f8c3b73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0610f9c04df8074c7e9b6a219af362",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "c337185b30abcaef92bf",
+    "url": "/my-portfolio/static/css/main.66021776.chunk.css"
+  },
+  {
+    "revision": "2563805ff02a7583cf0f",
+    "url": "/my-portfolio/static/js/2.bab48a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.bab48a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c337185b30abcaef92bf",
+    "url": "/my-portfolio/static/js/main.dfbd9cae.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "f99d28b8b5fb5c128ea99a49fad40d31",
+    "url": "/my-portfolio/static/media/118-macbook-edited.f99d28b8.svg"
+  }
+]);
