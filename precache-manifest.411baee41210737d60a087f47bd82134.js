0103cac3e9db41a5989d3fd567c7a160035375fa
Aaron Palas <aaron.palas@gmail.com>
2021-04-13 17:22:58 -0700

    Updates

---
+++ precache-manifest.411baee41210737d60a087f47bd82134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a67157d7487ebd21529cf9b46af54d",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "ceb36e7ab7b19ee71aaf",
+    "url": "/my-portfolio/static/css/main.66021776.chunk.css"
+  },
+  {
+    "revision": "2563805ff02a7583cf0f",
+    "url": "/my-portfolio/static/js/2.bab48a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.bab48a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb36e7ab7b19ee71aaf",
+    "url": "/my-portfolio/static/js/main.feee087a.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "f99d28b8b5fb5c128ea99a49fad40d31",
+    "url": "/my-portfolio/static/media/118-macbook-edited.f99d28b8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f757e2beb665a0b483b50a12a0a5747d.js"
+  "/my-portfolio/precache-manifest.411baee41210737d60a087f47bd82134.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
